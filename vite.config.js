f587bd00c505c352afe375db6d4746d35d0b4952
Modestas Razanauskas <modestas@Modestass-MacBook-Air.local>
2026-04-06 22:27:41 +0300

    Add plugins array to vite config for Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  plugins: [],
   server: { port: 3001, open: false },
   build: { outDir: 'dist', target: 'es2020' },
 });
